ae81ba2786dc5fa8f8645764debe1e5499ce8eef
Matt Harlum <matt@harlum.net>
2022-05-13 14:51:04 +0000

    Add License + Documentation

--- RTL/autoconfig.v
+++ RTL/autoconfig.v
@@ -1,3 +1,14 @@
+/*
+GottaGoFaZt3r
+Copyright 2022 Matthew Harlum
+
+GottaGoFaZt3r is licensed under a
+Creative Commons Attribution-ShareAlike 4.0 International License.
+
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+*/
+
 module Autoconfig (
     input match,
     output reg [3:0] addr_match,
